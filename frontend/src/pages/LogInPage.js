29aa19386afcd4c06d1adf7dbe6e3479cbce01c7
Jade Piramide <jadepiramidepogi@gmail.com>
2022-08-12 17:51:53 -0700

    Added forms and input boxes for username and password fields

--- frontend/src/pages/LogInPage.js
+++ frontend/src/pages/LogInPage.js
@@ -4,6 +4,21 @@ import ReactDOM from 'react-dom/client';
 const LogInPage = () => {
     return (
         <>
+        <h1> LOGIN </h1>
+
+        <form action='' method='GET'>
+            <p> Username </p>
+            <input type='text' />
+
+        </form>
+
+        <form action='' method='GET'>
+            <p> Password </p>
+            <input type='text' />
+
+        </form>
+
+
         </>
     );
 }
