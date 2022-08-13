9fc8c1b09b72c07714ab870c5e39b3ac2ff34d99
Jade Piramide <jadepiramidepogi@gmail.com>
2022-08-12 17:48:15 -0700

    Start work on LogInPage

---
+++ frontend/src/pages/LogInPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const LogInPage = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export { LogInPage };
